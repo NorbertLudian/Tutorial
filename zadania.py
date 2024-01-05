520b65d4628f35a17dfef98ed9b52d507d8e3ac7
NorbertLudian <norbert.ludian@gmail.com>
2024-01-05 07:16:17 +0100

    Create zadania.py

---
+++ zadania.py
@@ -0,0 +1,20 @@
+# ** ZADANIE 7
+# first_name = "John"
+# last_name = "Smith"
+# full_name = first_name + " " + last_name
+# message = f"Dear {first_name}, we inform you that you have purchased a ticket to travel to the island of Mauritius. Departure June 31 of this year. Have a passport at {full_name}. We are looking forward to seeing you!"
+# print(message)
+
+# ** ZADANIE 8
+# length = 2.75
+# width = 1.75
+# area = length * width
+# show = f"With width {width} and length {length} of the room, its area is equal to {area}"
+# print(show)
+
+# ** ZADANIE 9
+# Liczby zespolone
+# a = -2 + 3j
+# b = 4 + 2.1j
+# result = a + b
+# print(result)
